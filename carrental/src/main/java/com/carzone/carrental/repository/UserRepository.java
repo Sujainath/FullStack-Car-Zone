e7fca985206fcca55ae829c7d8ef0b9e3c276172
Sujainath J <sujainathsujainath@gmail.com>
2026-02-13 13:09:41 +0530

    Format UserRepository.java for consistency

--- carrental/src/main/java/com/carzone/carrental/repository/UserRepository.java
+++ carrental/src/main/java/com/carzone/carrental/repository/UserRepository.java
@@ -8,8 +8,9 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // Indha line-ah add pannunga, appo dhaan AuthController-la red color pogum
+
     Optional<User> findByEmail(String email);
 
     Optional<User> findByEmailOrUsername(String email, String username);
+
 }
